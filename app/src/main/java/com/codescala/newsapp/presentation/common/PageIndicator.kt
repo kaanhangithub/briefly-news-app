8821442d2192396c3778f16519c54a7686493559
Kaanhan <kaanhanokyay@gmail.com>
2024-07-09 14:18:15 -0400

    Added onboarding screen

---
+++ app/src/main/java/com/codescala/newsapp/presentation/common/PageIndicator.kt
@@ -0,0 +1,38 @@
+package com.codescala.newsapp.presentation.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import com.codescala.newsapp.presentation.Dimens.IndicatorSize
+import com.codescala.newsapp.ui.theme.BlueGray
+
+@Composable
+fun PageIndicator(
+    modifier: Modifier = Modifier,
+    pageSize: Int,
+    selectedPage: Int,
+    selectedColor: Color = MaterialTheme.colorScheme.primary,
+    unselectedColor: Color = BlueGray
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        repeat(pageSize) { index ->
+            Box(
+                modifier = Modifier.size(IndicatorSize)
+                    .clip(CircleShape)
+                    .background(color = if (index == selectedPage) selectedColor else unselectedColor)
+
+            )
+        }
+    }
+}
