e68424c9e59e880f6d310497ecff17e3d61636a8
Kaanhan <kaanhanokyay@gmail.com>
2024-07-09 11:37:34 -0400

    Added splash page

---
+++ app/src/main/java/com/codescala/newsapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.codescala.newsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.codescala.newsapp.ui.theme.NewsAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContent {
+            NewsAppTheme {
+
+            }
+        }
+    }
+}
