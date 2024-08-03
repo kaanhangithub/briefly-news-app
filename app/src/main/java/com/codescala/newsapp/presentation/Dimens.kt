50b31f73ab10b919d7a67980d35f3a183ca99d25
Kaanhan <kaanhanokyay@gmail.com>
2024-08-02 23:25:54 -0400

    Implemented home screen

--- app/src/main/java/com/codescala/newsapp/presentation/Dimens.kt
+++ app/src/main/java/com/codescala/newsapp/presentation/Dimens.kt
@@ -3,8 +3,14 @@ package com.codescala.newsapp.presentation
 import androidx.compose.ui.unit.dp
 
 object Dimens {
+    val ExtraSmallPadding = 3.dp
+    val ExtraSmallPadding2 = 4.dp
+    val SmallPadding = 6.dp
     val MediumPadding1 = 24.dp
     val MediumPadding2 = 30.dp
     val IndicatorSize = 14.dp
     val PageIndicatorWidth = 52.dp
+    val ArticleCardSize = 96.dp
+    val SmallIconSize = 11.dp
+    val IconSize = 20.dp
 }
